a624a50686598c7b93f59f305497e4c87ede39bd
Yush yetunde <yusufyetunde13@gmail.com>
2024-04-12 23:07:16 +0100

    image cropper

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import { useFileReader } from "./hooks";
+import { DragZone, ImageCropper } from "./components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [openCropper, setOpenCropper] = useState(false);
+    const [{ result, error, file }, setFile] = useFileReader({
+        method: "readAsDataURL",
+        accept: ["png", "jpg", "jpeg"],
+        maxFileSize: 5,
+    });
+
+    useEffect(() => {
+        if (result && !error.type) setOpenCropper(true);
+        else setOpenCropper(false);
+    }, [result, error.type]);
+
+    return (
+        <div className="relative min-h-screen flex flex-col items-center justify-center p-2 bg-black-100 font-poppins">
+          <h1 className="text-white-100 font-800 text-[48px]">Crop Image</h1>
+          <p className="text-white-50 text-[24px] mb-10">Quickly crop image for free</p>
+            <DragZone setFile={setFile} />
+            {(error.type === "fileType" || error.type === "fileSize") && <p className="text-red-500 font-medium">{error.msg}</p>}
+            <AnimatePresence>
+                {openCropper && (
+                    <ImageCropper
+                        imageSrc={result}
+                        file={file}
+                        setFile={setFile}
+                        setOpenCropper={setOpenCropper}
+                    />
+                )}
+            </AnimatePresence>
+        </div>
+    );
 }
 
 export default App;
